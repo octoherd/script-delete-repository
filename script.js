f5c2301eb2b750f04f8d63891eaf9ae0bdd9226c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-19 10:42:51 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * An Octoherd script to delete repositories
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
